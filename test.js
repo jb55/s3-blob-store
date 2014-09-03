87d1fd196e548280a824012dea8fe2adef743e18
William Casarin <bill@casarin.me>
2014-09-03 16:16:59 -0700

    simplify

--- test.js
+++ test.js
@@ -2,17 +2,20 @@
 
 var blobTests = require('abstract-blob-store/tests');
 var test = require('tape');
+var aws = require('aws-sdk');
 var s3 = require('./');
 
-var tokens = {
-  bucket: process.env.S3_BUCKET,
-  accessKey: process.env.S3_ACCESS_KEY,
-  secretKey: process.env.S3_SECRET_KEY
-}
+var client = new aws.S3({
+  accessKeyId: process.env.S3_ACCESS_KEY,
+  secretAccessKey: process.env.S3_SECRET_KEY
+})
 
 var common = {
   setup: function(t, cb) {
-    var store = s3(tokens)
+    var store = s3({
+      client: client,
+      bucket: process.env.S3_BUCKET
+    })
     cb(null, store)
   },
   teardown: function(t, store, blob, cb) {
